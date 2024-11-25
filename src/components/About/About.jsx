8d88cf485ba9d9d81b7234bf59dc07ae7b80079c
Karim B <boualam.karim94@gmail.com>
2024-11-25 01:36:36 +0100

    Ajout des composants et fichiers nécessaires

---
+++ src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.scss';
+
+
+const About=()=>{
+    return (
+        <section className="about">
+            <h1>About Me</h1>
+            <p> This is the About Page</p>
+        </section>
+    );
+}
+export default About;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.scss';
+
+
+const Home=()=>{
+    return (
+        <section className="home">
+            <h1>Welcome to MyPortoflio</h1>
+            <p> This is the Home Page</p>
+        </section>
+    );
+};
+export default Home;
