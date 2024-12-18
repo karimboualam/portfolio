bcdc74273f6ca5f141b8661b9e040e1f595b72da
Karim <boualam.karim94@gmail.com>
2024-12-18 01:43:11 +0100

    ajout de footer .jsx et style scss

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import "./Footer.scss";
+import { socialIcons, SosLinks } from "../../Data/Data";
+import { motion } from "framer-motion";
 
 const Footer = () => {
-    return (
-        <footer>
-            <p>&copy; 2024 My Portfolio. All rights reserved.</p>
-        </footer>
-    );
+  function getCurrentYear() {
+    const currentDate = new Date(); // Crée un nouvel objet Date avec la date actuelle
+    const currentYear = currentDate.getFullYear(); // Récupère l'année actuelle
+    return currentYear;
+  }
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{ duration: 1.5 }}
+      className="footer"
+    >
+      <div className="copyright">
+        <p>
+          Copyright&copy;{getCurrentYear()} All rights reserved.Made by{" "}
+          <span>Karim BOUALAM</span>
+        </p>
+      </div>
+      <div className="followMe">
+        <h4>Follow Me</h4>
+        <div className="stick"></div>
+        <div className="social_icons">
+          {socialIcons.map((socialIcon, index) => {
+            return (
+              <div key={index}>
+                <a
+                  href={SosLinks[index]}
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  {socialIcon}
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Footer;
